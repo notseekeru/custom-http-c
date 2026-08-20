cb240c7f6ca93139000b94c06bfd51a4b54d246f
Stephen Macabulos <myteseeker18@gmail.com>
2026-08-20 15:12:43 +0800

    refactor(main): improve sockaddr_in initialization and simplify send call

--- main.c
+++ main.c
@@ -8,7 +8,10 @@
 int main() {
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in addr = {AF_INET, htons(8080), {INADDR_ANY}};
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(8080);
+  addr.sin_addr.s_addr = INADDR_ANY;
 
   bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
 
@@ -19,7 +22,7 @@ int main() {
   while (1) {
     int client_fd = accept(socket_fd, 0, 0);
     if (client_fd >= 1) {
-      int bytes_sent = send(client_fd, msg, msg_len, 0);
+      send(client_fd, msg, msg_len, 0);
       shutdown(client_fd, 1);
     }
   }
