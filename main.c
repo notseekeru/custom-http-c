da93ecb131588564f0d13464d69e17fdf08ed73b
Stephen Macabulos <myteseeker18@gmail.com>
2026-08-20 12:41:21 +0800

    refactor(server): use send for full client message

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
-
+#include <sys/socket.h>
 
 int main() {
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr = {AF_INET, htons(8080), {INADDR_ANY}};
+  struct sockaddr_in addr = {AF_INET, htons(8080), {INADDR_ANY}};
 
-    bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+  bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
 
-    listen(socket_fd, 1);
+  listen(socket_fd, 1);
 
-    int client = accept(socket_fd, 0, 0);
+  int client_fd = accept(socket_fd, 0, 0);
 
-    write(client, "Hello\n", 6);
+  const char *msg = "Hello, Server!";
+  int msg_len = strlen(msg);
 
-    close(client);
-    close(socket_fd);
+  int bytes_sent = send(client_fd, msg, msg_len, 0);
+
+  shutdown(client_fd, 1);
+  shutdown(socket_fd, 1);
 }
