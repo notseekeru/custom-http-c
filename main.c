51cf781ce2eb43855a2a2ed40f2283bde6a678a8
Stephen Macabulos <myteseeker18@gmail.com>
2026-08-20 12:28:26 +0800

    feat(main): implement basic TCP server to accept connections and respond with a message

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
 
 int main() {
-    printf("Hello, world!\n");
-    return 0;
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr = {AF_INET, htons(8080), {INADDR_ANY}};
+
+    bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+
+    listen(socket_fd, 1);
+
+    int client = accept(socket_fd, 0, 0);
+
+    write(client, "Hello\n", 6);
+
+    close(client);
+    close(socket_fd);
 }
