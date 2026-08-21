9f4e549daf89bd0240abc59a1f13c77c71c17dd8
Stephen Macabulos <myteseeker18@gmail.com>
2026-08-21 15:37:07 +0800

    refactor(main): improve error handling for socket operations

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,25 +8,42 @@
 
 int main() {
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    perror("socket");
+    exit(1);
+  }
 
   struct sockaddr_in addr = {0};
   addr.sin_family = AF_INET;
   addr.sin_port = htons(8080);
   addr.sin_addr.s_addr = INADDR_ANY;
 
-  bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+  if (bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    exit(1);
+  }
 
-  listen(socket_fd, 1);
+  if (listen(socket_fd, 1) < 0) {
+    perror("listen");
+    exit(1);
+  }
+
   const char *msg = "Hello, Server!";
   int msg_len = strlen(msg);
 
   while (1) {
     int client_fd = accept(socket_fd, 0, 0);
-    if (client_fd >= 1) {
-      send(client_fd, msg, msg_len, 0);
-      shutdown(client_fd, 1);
+    if (client_fd < 0) {
+      perror("accept");
+      continue;
     }
+
+    send(client_fd, msg, msg_len, 0);
+
+    close(client_fd);
   }
 
+  // TODO: add proper graceful shutdown and cleanup code here
   shutdown(socket_fd, 1);
+  close(socket_fd);
 }
